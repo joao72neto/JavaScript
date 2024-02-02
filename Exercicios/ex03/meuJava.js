ea5c6745858880e5ccbe9bca0d06d8eaf204f840
João Salvador Neto <99670872+joao72neto@users.noreply.github.com>
2024-02-02 18:11:29 -0300

    Analisador de números finalizado

---
+++ Exercicios/ex03/meuJava.js
@@ -0,0 +1,60 @@
+var valores = []
+ 
+function adicionarVal(){
+    //Pegando elementos
+    var num = document.getElementById('num')
+    var sel = document.getElementById('selid')
+
+    //Verificando o intervalo
+
+    if(num.value >= 1 && num.value <= 100){
+
+        //adicionando valores
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        sel.appendChild(item)
+        valores.push(num.value)
+
+    }else if (num.value.length == 0){
+        window.alert('Digite um valor')
+    }else{
+        window.alert('Digite apenas valores entre 1 e 100')
+    }  
+
+    return valores
+
+}
+
+function analisarVal(){
+    //Pegando elementos
+    var resp = document.getElementById('resp')
+
+
+    //Analisando os dados
+    var qtd = valores.length
+    valores.sort()
+    var maior = valores[qtd-1]
+    var menor = valores[0]
+
+    //Somando e tirando a média
+    var soma = 0
+    
+    for(var i in valores){
+        soma += parseInt(valores[i])
+    }
+
+    var media = soma / qtd
+    
+
+    //Exibindo os dados
+    resp.innerHTML = `<p>Ao todo temos ${qtd} números cadastrados</p>`
+
+    resp.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+
+    resp.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+
+    resp.innerHTML += `<p>A soma de todos os valores é ${soma}</p>`
+
+    resp.innerHTML += `<p>A média dos valores é ${media}</p>`
+}
+
