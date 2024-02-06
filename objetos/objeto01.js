29dace795d4209b7db753560fdc9e5ca5c55897f
João Salvador Neto <99670872+joao72neto@users.noreply.github.com>
2024-02-06 17:53:16 -0300

    testando objetos

---
+++ objetos/objeto01.js
@@ -0,0 +1,11 @@
+let amigo = {
+nome:"joao", 
+sexo:"m", 
+idade:19,
+envelhecer(i=0){
+    console.log("Envelheceu")
+    this.idade += i
+}}
+
+amigo.envelhecer(10)
+console.log(`${amigo.nome} tem ${amigo.idade} anos de idade`)
