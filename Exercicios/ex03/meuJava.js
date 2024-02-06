696af8217cec62e74d44180c34ac5ed165c4f15b
João Salvador Neto <99670872+joao72neto@users.noreply.github.com>
2024-02-06 17:28:27 -0300

    Analisador de números feito

--- Exercicios/ex03/meuJava.js
+++ Exercicios/ex03/meuJava.js
@@ -1,19 +1,30 @@
 var valores = []
- 
+
 function adicionarVal(){
     //Pegando elementos
-    var num = document.getElementById('num')
+    var num = document.getElementById('num') 
     var sel = document.getElementById('selid')
 
     //Verificando o intervalo
 
     if(num.value >= 1 && num.value <= 100){
 
-        //adicionando valores
-        var item = document.createElement('option')
-        item.text = `Valor ${num.value} adicionado`
-        sel.appendChild(item)
-        valores.push(num.value)
+        if(valores.indexOf(num.value) == -1){
+            //adicionando valores
+            var item = document.createElement('option')
+            item.text = `Valor ${num.value} adicionado`
+            sel.appendChild(item)
+            valores.push(num.value)
+
+            //Limpando análise após adicionar um valor
+            resp.innerHTML = ''
+
+            //Limpando número anterior
+            num.value = ''
+            num.focus()
+        }else{
+            window.alert(`Valor ${num.value} já inserido`)
+        }
 
     }else if (num.value.length == 0){
         window.alert('Digite um valor')
@@ -22,7 +33,6 @@ function adicionarVal(){
     }  
 
     return valores
-
 }
 
 function analisarVal(){
@@ -45,16 +55,20 @@ function analisarVal(){
 
     var media = soma / qtd
     
-
     //Exibindo os dados
-    resp.innerHTML = `<p>Ao todo temos ${qtd} números cadastrados</p>`
+    if(valores.length != 0){
 
-    resp.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        resp.innerHTML = `<p>Ao todo temos ${qtd} números cadastrados</p>`
 
-    resp.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        resp.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
 
-    resp.innerHTML += `<p>A soma de todos os valores é ${soma}</p>`
+        resp.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
 
-    resp.innerHTML += `<p>A média dos valores é ${media}</p>`
+        resp.innerHTML += `<p>A soma de todos os valores é ${soma}</p>`
+
+        resp.innerHTML += `<p>A média dos valores é ${media}</p>`
+    }else{
+        window.alert("Insira um valor antes")
+    } 
 }
 
